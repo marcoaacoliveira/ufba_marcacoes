d146b4ec0fb49da6add1a065d9ae2ee3317bce21
Marco Oliveira <marcoaacoliveira@gmail.com>
2016-04-29 18:37:26 -0300

    Adicionado atributo $connection a Model, para tornar essa menos acoplada à Connection.

--- models/Model.php
+++ models/Model.php
@@ -4,17 +4,17 @@ class Model
 {
     protected $table;
     protected $connection;
-    /**
-     * Procura uma linha por $id
-     * @param $id
-     * @return mixed
-     */
     public function __construct()
     {
         $this->table = strtolower(get_class($this).'s');
         $this->connection = \Database\Connection::getInstance();
     }
 
+    /**
+     * Procura uma linha por $id
+     * @param $id
+     * @return mixed
+     */
     public function find($id){
         $sth = $this->connection->prepare("SELECT * FROM {$this->table} WHERE id=?");
         $sth->execute(array($id));
@@ -23,7 +23,6 @@ class Model
 
     /**
      * Procura todas as linhas
-     * @param $table
      * @return array
      */
     public function findAll() {
