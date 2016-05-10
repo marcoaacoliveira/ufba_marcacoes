9d713671c001da4ab4375d09f56e49c166637912
Marco Oliveira <marcoaacoliveira@gmail.com>
2016-05-10 14:13:24 -0300

    Adicionado padrão de código a classe User

--- models/User.php
+++ models/User.php
@@ -1,11 +1,15 @@
 <?php
+
 class User extends Model
 {
-    public function save(){
+    public function save()
+    {
         $this->securePassword();
         parent::save();
     }
-    private function securePassword() {
+
+    private function securePassword()
+    {
         $this->password = md5($this->password);
     }
 }
