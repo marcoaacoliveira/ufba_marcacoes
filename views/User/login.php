d327c88858fb2f606742a3df44121f895f546b7b
Hiolanda Murielle <hiolandamsm@gmail.com>
2016-05-11 17:30:58 -0300

    Adicionando css e javascript ao projeto.

--- views/User/login.php
+++ views/User/login.php
@@ -8,10 +8,19 @@ if (isset($_SESSION['log'])) {
 ?>
 
     <form action="/user/auth" method="post">
-        <label>Login</label>
-        <input name="login" type="text"/>
-        <label>Senha</label>
-        <input name="password" type="password"/>
-        <input type="submit"/>
+        <div id="centro-main">
+            <div id="centro-div">
+                <img src="/vendor/img/logo.png" alt="" width="50%" height="50%">
+                <p><input name="login" type="text" class="feedback-input form-input" placeholder="Usuário" id="name" required="required" /></p>
+                <p><input name="password" type="password" class="feedback-input form-input" placeholder="Senha" id="name" required="required"/></p>
+                <div class="submit">
+                    <input type="submit" value="Acessar" id="button"/>
+                    <div class="ease"></div>
+                </div>
+            </div>
+        </div>
     </form>
+  
+
+
 <?php require(dirname(dirname(__FILE__)) . '/includes/footer.php') ?>
--- views/includes/header.php
+++ views/includes/header.php
@@ -2,6 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="/vendor/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
     <title>Marcações de Consultas UFBA</title>
 </head>
 <body>
