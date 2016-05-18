da90d5096cbc40ee5585de15e19d6bb93e11e86a
Marco Oliveira <marcoaacoliveira@gmail.com>
2016-05-17 21:38:04 -0300

    Adicionado melhorias para tela de login.

--- views/User/login.php
+++ views/User/login.php
@@ -1,26 +1,75 @@
-<?php
-session_start();
-require(dirname(dirname(__FILE__)) . '/includes/header.php');
-if (isset($_SESSION['log'])) {
-    echo $_SESSION['log'];
-    unset($_SESSION['log']);
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>HOSPMEV - Login</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="/vendor/adminlte/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/vendor/adminlte/dist/css/AdminLTE.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href=/vendor/adminlte/plugins/iCheck/square/blue.css">
 
-    <form action="/user/auth" method="post">
-        <div id="centro-main">
-            <div id="centro-div">
-                <img src="/vendor/img/logo.png" alt="" width="50%" height="50%">
-                <p><input name="login" type="text" class="feedback-input form-input" placeholder="Usuário" id="name" required="required" /></p>
-                <p><input name="password" type="password" class="feedback-input form-input" placeholder="Senha" id="name" required="required"/></p>
-                <div class="submit">
-                    <input type="submit" value="Acessar" id="button"/>
-                    <div class="ease"></div>
-                </div>
-            </div>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-box-body">
+
+        <div class="register-logo">
+            <img src="/vendor/img/logo.png">
         </div>
-    </form>
 
+        <p class="login-box-msg">Logue-se para acessar o sistema</p>
 
+        <form action="/user/auth" method="post">
+            <div class="form-group has-feedback">
+                <input type="login" class="form-control" placeholder="Login">
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Senha">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="row">
+                <!-- /.col -->
+                <div class="col-xs-4">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">Login</button>
+                </div>
+                <!-- /.col -->
+            </div>
+        </form>
+    </div>
+    <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
 
-<?php require(dirname(dirname(__FILE__)) . '/includes/footer.php') ?>
+<!-- jQuery 2.2.0 -->
+<script src="/vendor/adminlte/plugins/jQuery/jQuery-2.2.0.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="/vendor/adminlte/bootstrap/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="/vendor/adminlte/plugins/iCheck/icheck.min.js"></script>
+<script>
+    $(function () {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
+    });
+</script>
+</body>
+</html>
